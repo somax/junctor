2770ea15d32890ebb2cc5390d2a7077383f0bcc4
somax <somaxj@163.com>
2017-03-16 08:53:43 +0800

    断线重连

--- client.js
+++ client.js
@@ -1,48 +1,50 @@
 const net = require('net');
 const util = require('util');
+const rc = require('reconnect-net');
 
 let clientId = process.argv[2] || 'client1';
 let channelId = process.argv[3] || 'channel1';
 let host = process.argv[4] || '0.0.0.0';
 let port = process.argv[5] || 8124;
 
-const client = net.connect({ host:host, port: port }, () => {
-    // 'connect' listener
-    util.log('connected to server!');
-
-    client.write(clientId+':'+channelId);
+let client;
 
 
-    // let intervalId = setInterval(() => {
-    //     client.write(Date.now().toString()+'\n')
-    // }, 10000);
-
-    // client.on('end', () => {
-    //     clearInterval(intervalId);
-    // })
+rc(function(_client){
+    client = _client;
+    util.log('...reconnected');
+    _client.write(clientId + ':' + channelId);
 
 
+    let intervalId = setInterval(() => {
+        _client.write(Date.now().toString()+'\n');
+    }, 10000);
+
+    _client.on('close', () => {
+        clearInterval(intervalId);
+    });
+
+    _client.on('data', (data) => {
+
+        util.log(data.toString());
+        // client.end();
+    });
+}).connect(port,host);
+
+
+process.on('uncaughtException', function(err) {
+    util.log(err);
 });
-client.on('data', (data) => {
-    util.log(data.toString());
-    // client.end();
-});
-client.on('end', () => {
-    util.log('disconnected from server');
 
-
-});
-
-
-// ---------------
+// 交互输入
 const readline = require('readline');
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  // output: process.stdout
+    input: process.stdin,
+    // output: process.stdout
 });
 
 rl.on('line', (input) => {
-  client.write(input)
+    client.write(input);
 
 });
